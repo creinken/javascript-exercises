df7bd8470d97464c45edd8798dcf60f8b0027c41
creinken <reinkendigital@gmail.com>
2025-05-18 21:56:27 -0500

    Finished exercise 09 sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let start, end;
+    if (a < 0 || b < 0 || typeof a != "number" || typeof b != "number" || (a % 1) != 0 || (b % 1) != 0) {
+        return "ERROR"
+    } else if (a > b) {
+        start = b;
+        end = a;
+    } else if (a < b) {
+        start = a;
+        end = b;
+    }
+    result = start;
+    for (let i = result + 1; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
