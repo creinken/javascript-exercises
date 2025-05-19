45c55db922c1843d9a89dbd75b3ee6607676e140
creinken <reinkendigital@gmail.com>
2025-05-18 21:01:15 -0500

    Finished excercise 06 repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if (count < 0)
+        return "ERROR"
 
+    result = "";
+    for (let i = 0; i < count; i++) {
+        result += str;    
+    }
+
+    return result;
 };
 
 // Do not edit below this line
