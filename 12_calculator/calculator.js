159ae2483e8c8a7a056c3396dd08d87aac7be901
creinken <reinkendigital@gmail.com>
2025-05-29 21:28:14 -0500

    Finish excersise 12 calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let result = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    result *= numbers[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(count) {
+	let result = 1;
+  for (let i = 1; i <= count; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
