fcb1c9800042976c835e0ec0195b93c0db8bbd54
creinken <reinkendigital@gmail.com>
2025-05-29 23:23:29 -0500

    Finish excercise 16 findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    people.forEach((person) => {
+        if (getAge(oldest) < getAge(person)) {
+            oldest = person;
+        }
+    });
+    return oldest;
 };
+
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        return 2025 - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
