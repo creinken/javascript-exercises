eff88f4664ae9efdbb2525b167ad959211600952
creinken <reinkendigital@gmail.com>
2025-05-29 21:48:25 -0500

    Finish excercise 13 palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+    let forwards = palindrome.split('').filter(char => { return /[a-zA-Z0-9]/.test(char); }).join('').toLowerCase();
+    let backwards = forwards.split('').reverse().join('');
 
+    return forwards == backwards;
 };
 
 // Do not edit below this line
