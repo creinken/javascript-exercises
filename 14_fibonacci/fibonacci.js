5674b7dd64439e25159c9942645912c5e2fcbe62
creinken <reinkendigital@gmail.com>
2025-05-29 22:45:43 -0500

    Finish excercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let seqNumber = parseInt(number);
+    let current = 0;
+    let a = 0;
+    let b = 1;
 
+    if (seqNumber == 0) {
+        return 0;
+    } else if (seqNumber < 0) {
+        return 'OOPS';
+    } else {
+        for (let i = 2; i <= seqNumber; i++) {
+            current = a + b;
+            a = b;
+            b = current;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
